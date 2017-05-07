55e3f69a5b2c9ea196d0b50c000b7ee3bfbca376
James Harrison <jharrison23@knights.ucf.edu>
2017-05-07 03:28:57 -0400

    testing out the multiple messages again

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ restService.post('/myecho', function(req, res) {
         UserSent: speech
     });
 
-
+    sendMessage();
 
     return res.json({
         speech: speech,
@@ -61,6 +61,37 @@ restService.post('/myecho', function(req, res) {
     });
 
 });
+
+
+function sendMessage()
+{
+    restService.post('/myecho', function(req, res) {
+    var speech = req.body.result && req.body.result.parameters && 
+                 req.body.result.parameters.echoText ? 
+                 req.body.result.parameters.echoText : "Seems like some problem. Speak again."
+  
+    
+    // Pushes the text from the user to the firebase database
+    // pushes into the the AnaBot -> Messages table.
+
+   // .push creates a push key, the push key shows up as the weird string
+   // under messages looks like an id, push keys are critical to firebase, 
+   // theyre like time stamps with alot of randomness to avoid collision
+    messagesRef.push({
+        UserSent: "second message"
+    });
+
+    sendMessage();
+
+    return res.json({
+        speech: "second message",
+       // displayText: speech,
+        source: 'echo-web-hook'
+        
+    });
+
+});
+}
 
 
 restService.listen((process.env.PORT || 8000), function() {
